8edd2ad849ba45150405980dc5967cfeb10e01f3
haonx <qtvhao@gmail.com>
2018-01-03 07:32:30 +0700

    add method get Course

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -3,5 +3,8 @@ module.exports = ["$http", "RESOURCES_URL", function ($http, RESOURCES_URL) {
     courseService.all = function () {
         return $http.get(RESOURCES_URL+'courses/items.json');
     };
+    courseService.get = function (course) {
+        return $http.get(RESOURCES_URL+'courses/'+ course.slug+'.json');
+    };
     return courseService;
 }];
