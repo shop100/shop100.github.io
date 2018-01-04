8ab158832f729ef88bf2be7d6e5dde716f335e3d
haonx <qtvhao@gmail.com>
2018-01-04 21:00:43 +0700

    listing lessons in Course

--- src/controllers/CoursesModuleShowController.js
+++ src/controllers/CoursesModuleShowController.js
@@ -1,6 +1,13 @@
 module.exports = [
     "$scope",
     "$stateParams",
-    function (scope, $stateParams) {
+    "CourseService",
+    function (scope, $stateParams, CourseService) {
+        CourseService.get({slug:$stateParams.slug}).then(function (resp) {
+            scope.course = resp.data.course;
+            scope.module = scope.course.modules.filter(function (module) {
+                return Number($stateParams.module_id) === Number(module.id);
+            })[0];
+        });
     }
 ];
