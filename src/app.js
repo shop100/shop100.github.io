11bca62a4bb33ec9cdbd03503dbb99bdd817081c
haonx <qtvhao@gmail.com>
2018-01-05 23:24:07 +0700

    bbDecode

--- src/app.js
+++ src/app.js
@@ -7,10 +7,35 @@ angular
         "AppServices",
         "AppControllers"
     ])
-    .run(["$rootScope",function ($rootScope, $mdSidenav, $mdMedia) {
+    .run(["$rootScope", function ($rootScope, $mdSidenav, $mdMedia) {
         $rootScope.openMenu = function () {
-            $mdSidenav('left').toggle();
-        }
+            $mdSidenav("left").toggle();
+        };
     }])
-;
+    .directive("bbDecode", function ($sce) {
+        return {
+            template: "<div ng-bind-html='html'></div>",
+            scope: {
+                content: "="
+            },
+            link: function (scope) {
+                scope.$watch('content', function () {
+                    if(!scope.content){
+                        return;
+                    }
+                    var html = scope.content;
+                    html = html.replace(/\[b]/gi,'<strong>');
+                    html = html.replace(/\[\/b]/gi,'</strong>');
+                    html = html.replace(/\[h1]/gi,'<h1>');
+                    html = html.replace(/\[\/h1]/gi,'</h1>');
 
+                    html = html.replace(/\n/gi,'<br/>');
+
+                    html = html.replace(/\[note]/gi,'<div class="noteBlock"><span class="note">');
+                    html = html.replace(/\[\/note]/gi,'</span></div>');
+
+                    scope.html = $sce.trustAsHtml(html);
+                });
+            }
+        };
+    });
