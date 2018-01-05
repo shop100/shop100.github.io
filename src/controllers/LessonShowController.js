e451a9ded1e06c5388326e0f7e569a03cbdaec94
haonx <qtvhao@gmail.com>
2018-01-05 21:01:58 +0700

    add Quiz selector

--- src/controllers/LessonShowController.js
+++ src/controllers/LessonShowController.js
@@ -5,12 +5,30 @@ module.exports = [
     function (scope, LessonService, params) {
         LessonService.get(params.course_id, params.module_id, params.lesson_id).then(function (resp) {
             scope.lesson = resp.data.lesson;
+            scope.selectLessonText(scope.lesson.quizzes[0]);
         });
+        scope.$watch('lesson.quizzes|json', function () {
+            if (scope.lesson && scope.lesson.quizzes) {
+                scope.quizSelected = scope.lesson.quizzes.filter(function (quiz) {
+                    return quiz.selected;
+                })[0];
+            }
+        });
+        scope.deSelectAllItem = function () {
+            scope.lesson.quizzes = scope.lesson.quizzes.map(function (quiz) {
+                quiz.selected = false;
+                return quiz;
+            });
+        };
         scope.selectLessonText = function (quiz) {
-
+            scope.deSelectAllItem();
+            quiz.selected = true;
+            scope.view = 'text';
         };
         scope.selectQuiz = function (quiz) {
-
+            scope.deSelectAllItem();
+            quiz.selected = true;
+            scope.view = 'quiz';
         }
     }
 ];
