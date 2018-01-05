2e67edcfe4508a2a37be291b200a801a129ded1b
haonx <qtvhao@gmail.com>
2018-01-05 18:44:22 +0700

    reformat

--- src/controllers.js
+++ src/controllers.js
@@ -6,49 +6,49 @@ angular
         "$urlRouterProvider",
         "$stateProvider",
         function ($urlRouterProvider, $stateProvider) {
-        var homeState = {
-            name: 'home',
-            url: '/',
-            template: '',
-            controller:[
-                "$state",
-                function ($state) {
-                    $state.go('courses');
-                }
-            ]
-        };
+            var homeState = {
+                name: 'home',
+                url: '/',
+                template: '',
+                controller: [
+                    "$state",
+                    function ($state) {
+                        $state.go('courses');
+                    }
+                ]
+            };
 
-        var coursesState = {
-            name: 'courses',
-            url: '/courses',
-            controller: require('./controllers/CoursesController.js'),
-            template: require('./pages/courses.html')
-        };
+            var coursesState = {
+                name: 'courses',
+                url: '/courses',
+                controller: require('./controllers/CoursesController.js'),
+                template: require('./pages/courses.html')
+            };
 
-        var courseState = {
-            name: 'courses.show',
-            url: '/:slug',
-            views:{
-                "@": {
-                    controller: require('./controllers/CoursesShowController.js'),
-                    template: require('./pages/course-show.html')
+            var courseState = {
+                name: 'courses.show',
+                url: '/:slug',
+                views: {
+                    "@": {
+                        controller: require('./controllers/CoursesShowController.js'),
+                        template: require('./pages/course-show.html')
+                    }
                 }
-            }
-        };
-        var courseModuleShowState = {
-            name: 'courses.show.module',
-            url: '/:course_id/modules/:module_id',
-            views:{
-                "@": {
-                    controller: require('./controllers/CoursesModuleShowController.js'),
-                    template: require('./pages/course-module-show.html')
+            };
+            var courseModuleShowState = {
+                name: 'courses.show.module',
+                url: '/:course_id/modules/:module_id',
+                views: {
+                    "@": {
+                        controller: require('./controllers/CoursesModuleShowController.js'),
+                        template: require('./pages/course-module-show.html')
+                    }
                 }
-            }
-        };
+            };
 
-        $stateProvider.state(homeState);
-        $stateProvider.state(coursesState);
-        $stateProvider.state(courseState);
-        $stateProvider.state(courseModuleShowState);
-        $urlRouterProvider.otherwise('/');
-    }]);
+            $stateProvider.state(homeState);
+            $stateProvider.state(coursesState);
+            $stateProvider.state(courseState);
+            $stateProvider.state(courseModuleShowState);
+            $urlRouterProvider.otherwise('/');
+        }]);
