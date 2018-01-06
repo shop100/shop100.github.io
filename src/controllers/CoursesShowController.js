a0f625d86fcfa2721502e92b6ec3cfbce948eab7
haonx <qtvhao@gmail.com>
2018-01-06 20:24:49 +0700

    fix CourseService

--- src/controllers/CoursesShowController.js
+++ src/controllers/CoursesShowController.js
@@ -3,10 +3,26 @@ module.exports = [
     "CourseService",
     "$stateParams",
     "$state",
-    function (scope, CourseService, $stateParams, state) {
+    "$rootScope",
+    function (scope, CourseService, $stateParams, state, rootScope) {
         CourseService.get({slug:$stateParams.slug}).then(function (resp) {
             scope.course = resp.data.course;
             scope.course.slug = $stateParams.slug;
+        });
+        CourseService.all().then(function (resp) {
+            if (!rootScope.courses) {
+                rootScope.courses = resp.data;
+            }
+
+            rootScope.courses = rootScope.courses.map(function (course) {
+                CourseService.get(course).then(function (resp) {
+                    var respData = resp.data.course;
+                    course.modules = respData.modules;
+                    course.name = respData.name;
+                });
+
+                return course;
+            });
         });
         scope.stateGo = function (stateName, stateParams) {
             state.go(stateName, stateParams);
