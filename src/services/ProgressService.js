239cb5c2aaaa4013a923a53b0443a88a2e054cb4
haonx <qtvhao@gmail.com>
2018-01-06 17:51:20 +0700

    pipe progressesService

--- src/services/ProgressService.js
+++ src/services/ProgressService.js
@@ -1,11 +1,35 @@
 module.exports = [
     function () {
         var progressService = {};
+        if(!localStorage.progresses) {
+            localStorage.progresses = '{}';
+        }
+        progressService.pipe = function (handle) {
+            var progresses = JSON.parse(localStorage.progresses);
+            progresses = handle(progresses);
+            localStorage.progresses = JSON.stringify(progresses);
+        };
         progressService.done = function (type, id) {
-            localStorage.progresses[type][id] = {status:true};
+            progressService.pipe(function (progresses) {
+                if(!progresses[type]){
+                    progresses[type] = {};
+                }
+                progresses[type][id] = {status:true};
+
+                return progresses;
+            })
         };
         progressService.isDone = function (type, id) {
-            return localStorage.progresses[type][id].status;
+            var done;
+            done = false;
+            progressService.pipe(function (progresses) {
+                if(!progresses[type]){
+                    progresses[type] = {};
+                }
+                done = progresses[type][id].status;
+            });
+
+            return done;
         };
         return progressService;
     }
