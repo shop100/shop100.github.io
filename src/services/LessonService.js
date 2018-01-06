3de14e44d0e32e33393c52e12c6d80facb24f883
haonx <qtvhao@gmail.com>
2018-01-06 17:55:32 +0700

    add Lesson progresses

--- src/services/LessonService.js
+++ src/services/LessonService.js
@@ -1,10 +1,19 @@
 module.exports = [
     "$http",
     "RESOURCES_URL",
-    function ($http, RESOURCES_URL) {
+    "ProgressService",
+    function ($http, RESOURCES_URL, ProgressService) {
         var lessonService = {};
         lessonService.get = function (course_id, module_id, lesson_id) {
             return $http.get(RESOURCES_URL + 'courses' + '/' + course_id + '/' + 'modules' + '/' + module_id + '/' + 'lessons' + '/' + lesson_id + '.json');
+        };
+        lessonService.done = function (lesson, quiz) {
+            var progressType = 'lessonQuiz_' + lesson.id;
+            return ProgressService.done(progressType, quiz.id);
+        };
+        lessonService.isDone = function (lesson, quiz) {
+            var progressType = 'lessonQuiz_' + lesson.id;
+            return ProgressService.done(progressType, quiz.id);
         };
         return lessonService;
     }
