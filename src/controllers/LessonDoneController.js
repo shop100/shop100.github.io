d8c6b21e74ee2775cc1a4c39e5fd4de9fb450b24
haonx <qtvhao@gmail.com>
2018-01-09 20:44:50 +0700

    turn to next lesson, turn to next module

--- src/controllers/LessonDoneController.js
+++ src/controllers/LessonDoneController.js
@@ -6,6 +6,46 @@ module.exports = [
     "$state",
     "$mdToast",
     function (scope, LessonService, CourseService, params, state, toast) {
-        console.log(params);
+        CourseService.get({slug: params.slug}).then(function (resp) {
+            var course = resp.data.course;
+            var modules = course.modules;
+            var currentModule = modules.filter(function(module){return Number(module.id)===Number(params.module_id)})[0];
+            var currentLesson = currentModule.lessons.filter(function(lesson){return Number(lesson.id)===Number(params.lesson_id)})[0];
+            var currentLessonPosition;
+            currentLessonPosition = currentModule.lessons.indexOf(currentLesson);
+            var currentModulePosition;
+            currentModulePosition=course.modules.indexOf(currentModule);
+            var nextLesson = currentModule.lessons[currentLessonPosition+1];
+            var nextModule = modules[currentModulePosition+1];
+            var toastTextContent = 'Chính xác. Chúc mừng bạn đã hoàn thành tiết học';
+            if(nextLesson){
+                state.go('courses.show.module.lesson', {
+                    lesson_id: nextLesson.id,
+                    module_id: currentModule.id,
+                    course_id: course.id,
+                    slug: course.alias,
+                });
+            }else{
+                if(nextModule) {
+                    state.go('courses.show', {
+                        slug:params.slug,
+                    });
+                    toastTextContent = 'Chính xác. Chúc mừng bạn đã hoàn thành học phần.';
+                } else {
+                    state.go('courses.show', {
+                        slug:params.slug,
+                    });
+                    toastTextContent = 'Chính xác. Chúc mừng bạn đã hoàn thành khóa học ' + course.name;
+                }
+            }
+
+            toast.show(
+                toast.simple()
+                    .textContent(toastTextContent)
+                    .highlightAction(true)
+                    .highlightClass('md-success')
+                    .position('bottom right')
+            );
+        });
     }
 ];
