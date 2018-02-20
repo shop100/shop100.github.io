00407369f38a461d25559d6a66a3f2f557a88917
haonx <qtvhao@gmail.com>
2018-02-20 11:08:51 +0700

    allow mixing up lessons

--- src/services/LessonService.js
+++ src/services/LessonService.js
@@ -5,7 +5,7 @@ module.exports = [
     function ($http, RESOURCES_URL, ProgressService) {
         var lessonService = {};
         lessonService.get = function (course_id, module_id, lesson_id) {
-            return $http.get(RESOURCES_URL + 'courses' + '/' + course_id + '/' + 'modules' + '/' + module_id + '/' + 'lessons' + '/' + lesson_id + '.json');
+            return $http.get(RESOURCES_URL + 'lessons' + '/' + lesson_id + '.json');
         };
         var progressType = 'lessonQuiz';
         lessonService.done = function (quiz) {
