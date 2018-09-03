2f63648cf4c397bf36ad04f127b9f0cbe37c442f
haonx <qtvhao@gmail.com>
2018-09-03 11:31:34 +0700

    refactor

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -3,13 +3,13 @@ module.exports = [
     "RESOURCES_URL",
     "ProgressService",
     function ($http, RESOURCES_URL, ProgressService) {
-    var courseService = {};
-    courseService.all = function () {
-        return $http.get(RESOURCES_URL+'courses/items.json');
-    };
-    courseService.get = function (course) {
-        return $http.get(RESOURCES_URL+'courses/'+ course.slug+'.json');
-    };
+        var courseService = {};
+        courseService.all = function () {
+            return $http.get(RESOURCES_URL + 'courses/items.json');
+        };
+        courseService.get = function (course) {
+            return $http.get(RESOURCES_URL + 'courses/' + course.slug + '.json');
+        };
         var lessonProgressType = 'lesson';
         courseService.doneLesson = function (lesson) {
             ProgressService.done(lessonProgressType, lesson.id);
@@ -17,5 +17,5 @@ module.exports = [
         courseService.isDoneLesson = function (lesson) {
             return ProgressService.isDone(lessonProgressType, lesson.id);
         };
-    return courseService;
-}];
+        return courseService;
+    }];
